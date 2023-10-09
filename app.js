9d08eacf16376536cb4d0fc962e8b37c490ffb6f
ankit258 <123321233+Ankit-Panday258@users.noreply.github.com>
2023-10-09 18:14:09 +0530

    add morgan Package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from'express';
 import cors from'cors';
 import cookieParser from'cookie-parser';
+import morgan from 'morgan';
 const app = express();
 app.use(express.json());
  
@@ -10,6 +11,8 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
+
+app.use(morgan('dev'))
 
 app.use('/ping', function(req,res){
     res.send('Pong🥰🚩');
