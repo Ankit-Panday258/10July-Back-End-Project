c201b7b3552d8e737abd98a2539c6b44f2814c6e
ankit258 <123321233+Ankit-Panday258@users.noreply.github.com>
2023-10-11 15:41:33 +0530

    add connectionToDB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import {config} from 'dotenv';
 config()
 import app from'./app.js';
-import connectionToDB from './controuers/dbConnection.js';
+import connectionToDB from './config/dbConnection.js';
 
 const PORT = process.env.POST|| 5010
 app.listen(PORT ,async ()=>{
