8fb42a0b1df036284184d3e299d09e696b3263c4
ankit258 <123321233+Ankit-Panday258@users.noreply.github.com>
2023-10-11 12:12:40 +0530

    create  router

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+//register
+router.post('./register' , register);
+//login
+router.post('./login' , login);
+//logout
+router.get('./logout' , logout);
+//getProfile
+router.get('./me' , getProfile);
+
+
+
+export default Router;
